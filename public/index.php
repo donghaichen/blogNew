c5bda20c349480eb7e69a94353449095f8b6c4ad
donghaichen <384560806@qq.com>
2018-06-09 16:11:27 +0800

    gitignore

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: donghai
+ * Date: 18-6-6
+ * Time: 下午1:41
+ */
+// Autoload 自动载入
+require '../bootstrap.php';
+
+// 路由配置
+//require '../config/routes.php';

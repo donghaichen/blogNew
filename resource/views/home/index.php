e30fd71728d34dd6f22f92ddbb7da0db3bd78226
donghaichen <chendonghai888@gmail.com>
2018-06-20 16:07:20 +0800

    gitalk

--- resource/views/home/index.php
+++ resource/views/home/index.php
@@ -46,6 +46,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <link rel="stylesheet" href="//unpkg.com/docsify/lib/themes/vue.css" title="vue">
+    <link rel="stylesheet" href="//unpkg.com/gitalk/dist/gitalk.css">
     <style>
         nav.app-nav li ul {
             min-width: 100px;
@@ -100,7 +101,7 @@
         },
         formatUpdated: '{MM}/{DD} {HH}:{mm}',
     }
-</script>
+</script> https://github.com/donghaichen/blogNew.git
 <script src="//unpkg.com/docsify/lib/docsify.js"></script>
 <script src="//unpkg.com/docsify/lib/plugins/search.min.js"></script>
 <script src="//unpkg.com/prismjs/components/prism-bash.min.js"></script>
@@ -109,5 +110,18 @@
 <script src="//unpkg.com/prismjs/components/prism-php.min.js"></script>
 <script src="//unpkg.com/prismjs/components/prism-sql.min.js"></script>
 <script src="//unpkg.com/prismjs/components/prism-javascript.min.js"></script>
+<script src="//unpkg.com/docsify/lib/plugins/gitalk.min.js"></script>
+<script src="//unpkg.com/gitalk/dist/gitalk.min.js"></script>
+<script>
+    const gitalk = new Gitalk({
+        clientID: '0e9e499be91be3c0dd44',
+        clientSecret: 'c5dead06fc49d4327f23c2f3dab9ea5ea50f7fa6',
+        repo: 'gitalk',
+        owner: 'gitalk',
+        admin: ['booxood', 'mamboer'],
+        id: 'Demo',
+        distractionFreeMode: true
+    })
+</script>
 </body>
 </html>

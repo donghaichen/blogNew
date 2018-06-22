0e44de174473c7134e30b0e85fc637023149402e
donghaichen <384560806@qq.com>
2018-06-22 17:52:29 +0800

    onhashchange

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -17,6 +17,7 @@ class HomeController extends Controller
 {
     public function index()
     {
+        var_dump($_ENV);
         var_dump(env('DB_HOST'));
         exit();
         return view('home/index', []);

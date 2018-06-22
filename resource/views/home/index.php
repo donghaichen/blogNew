4e737b5d952a10836b2becdee7182453a5bf85ae
donghaichen <384560806@qq.com>
2018-06-22 11:02:06 +0800

    onhashchange

--- resource/views/home/index.php
+++ resource/views/home/index.php
@@ -150,30 +150,12 @@
         var detect = function(){
             if(that.oldHash!=window.location.hash){
                 alert("HASH CHANGED - new has" + window.location.hash);
-                return window.location.hash;
                 that.oldHash = window.location.hash;
             }
         };
         this.Check = setInterval(function(){ detect() }, 100);
+        return window.location.hash;
     }
-
-
-
-
-    //url变化监听器
-//    if( ('onhashchange' in window) && ((typeof document.documentMode==='undefined') || document.documentMode==8)) {
-//        // 浏览器支持onhashchange事件
-//        window.onhashchange = hashChangeFire;  // TODO，对应新的hash执行的操作函数
-//    } else {
-//        // 不支持则用定时器检测的办法
-//        setInterval(function() {
-//            // 检测hash值或其中某一段是否更改的函数， 在低版本的iE浏览器中通过window.location.hash取出的指和其它的浏览器不同，要注意
-//            var ischanged = isHashChanged();
-//            if(ischanged) {
-//                hashChangeFire();  // TODO，对应新的hash执行的操作函数
-//            }
-//        }, 150);
-//    }
 </script>
 </body>
 </html>
